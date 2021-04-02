1dfe3fb0da71c7ad7d15829e6042660d37c186d0
apple <ponejirasuk@gmail.com>
2021-04-02 22:53:34 +0700

    ex

--- project/ex.py
+++ project/ex.py
@@ -32,22 +32,31 @@ if __name__ == '__main__':
     gray = cv2.cvtColor(result, cv2.COLOR_BGR2GRAY)
     _, thres = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     contours, _ = cv2.findContours(thres, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
+    # cv2.drawContours(img, contours, -1, (0, 255, 0), 3)
 
     MAXS = find_max_list(contours)
     HEIGT, WIDTH = gray.shape
+
+    max_rec = 0
+    box_rec = None
 
     for cnt in contours:
         area = cv2.contourArea(cnt)
         if area < MAXS:
             continue
         x, y, w, h = cv2.boundingRect(cnt)
-        if y > HEIGT/2 and w > h:
-            cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
-            print([x,y,w,h])
+        if y > HEIGT/2 and w > h and (w+h) >= max_rec \
+                and (x >= WIDTH/3) and (x <= WIDTH - WIDTH/3):
+            max_rec = (w+h)
+            box_rec = [x,y,w,h]
+                
+    x, y, w, h = box_rec
+    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 3)
 
-    cv2.namedWindow('w > h', cv2.WINDOW_NORMAL)
-    cv2.imshow('w > h', img)
+    cv2.namedWindow('max rectangle', cv2.WINDOW_NORMAL)
+    cv2.imshow('max rectangle', img)
 
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
+# and (w+h) >= max_rec
